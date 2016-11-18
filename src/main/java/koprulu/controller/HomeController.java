f77e0e3c3e8d4e0453fcf7af7a24810e4891e6d6
AliKOPRULU <aliperk@hotmail.com>
2016-11-18 16:53:32 +0300

    Iaea first commit

--- src/main/java/koprulu/controller/HomeController.java
+++ src/main/java/koprulu/controller/HomeController.java
@@ -4,5 +4,6 @@ package koprulu.controller;
  * Created by Ali on 18.11.2016.
  */
 public class HomeController {
-    //Ali Second test
+    //Ali Second test çalışmadı
+    //config değiştirilip yeniden :)
 }
